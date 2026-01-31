73c426833b8ef4864c13cc92d3777cbab7c6264b
chickendrop89 <chickendrop89@gmail.com>
2026-01-31 13:38:29 +0100

    vendorsetup.sh: bring back original KSU for this device
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,6 +54,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ];
     export FOX_DELETE_INITD_ADDON=1
 
     # KernelSU-Next / SukiSu support
+    export FOX_ENABLE_KERNELSU_SUPPORT=1
     export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
     export FOX_ENABLE_SUKISU_SUPPORT=1
 
