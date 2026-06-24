715530fdd97e764f38ad8b1a7ac4c371b0bd054e
chickendrop89 <chickendrop89@gmail.com>
2026-06-24 09:41:55 +0200

    pre_rom_flash.sh: remove /metadata dir prep
    * It's making the custom ROM flashes fail with various errors
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- recovery/root/system/bin/pre_rom_flash.sh
+++ recovery/root/system/bin/pre_rom_flash.sh
@@ -8,44 +8,9 @@ LOGMSG() {
 	echo "I:$1" >> /tmp/recovery.log;
 }
 
-do_dir_prep() {
-    recovery_cache="/data/cache/recovery/"
-    metadata_entries="
-        bootstat:0770:system:log
-        ota:0750:root:system
-        ota/snapshots:0750:root:system
-        gsi:0750:root:system
-        gsi/ota:0750:root:system
-        gsi/dsu:0750:root:system
-        staged-install:0770:root:system
-        userspacereboot:0770:root:system
-        watchdog:0770:root:system
-    "
-
-    mkdir -p "$recovery_cache"
-
-    if mountpoint -q /metadata || mount /metadata 2>/dev/null; then
-        for entry in $metadata_entries; do
-            path=$(echo "$entry" | cut -d: -f1)
-            mode=$(echo "$entry" | cut -d: -f2)
-            owner=$(echo "$entry" | cut -d: -f3)
-            group=$(echo "$entry" | cut -d: -f4)
-            
-            full_path="/metadata/$path"
-
-            mkdir -p "$full_path"
-            chmod "$mode" "$full_path"
-            chown "$owner:$group" "$full_path"
-        done
-    else
-        LOGMSG "Failed to mount metadata, aborting";
-        exit 1
-    fi
-}
-
 do_prop_prep() {
     LOGMSG "Resetting SPL date to prevent data wipe..."
-    resetprop twrp.temp.security_patch $(resetprop ro.build.version.security_patch)
+    resetprop twrp.temp.security_patch "$(resetprop ro.build.version.security_patch)"
 
     resetprop ro.build.version.security_patch 2025-07-30
     resetprop ro.vendor.build.security_patch 2025-07-30
@@ -72,7 +37,6 @@ backup_fox() {
 }
 
 LOGMSG "Running pre-ROM-flash script...";
-do_dir_prep;
 do_prop_prep;
 backup_fox "$@";
 exit 0;
